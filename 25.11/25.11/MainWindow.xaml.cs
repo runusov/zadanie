56ad8910d4455d13aae24ddc99370beced4dedfc
Ruslan Yunusov <runusov48@gmail.com>
2023-11-26 22:50:15 +0500

    xaml

---
+++ 25.11/25.11/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _25._11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Неверно!", "My App", MessageBoxButton.OK ,MessageBoxImage.Error);
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, Tb.Text);
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files(*.txt)|*.*";
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                img1.Source = new BitmapImage(new Uri(fileName));
+            }
+            openFileDialog.Filter = "Images files(*.png;*jpeg)|*.png;*.jpeg|All files(*.*)|*.*";
+        }
+    }
+}
